a6fbaf635dcb6723f796928308534525796faf5f
aaevers <aidenevers@yahoo.com>
2026-08-08 16:11:13 -0500

    Added exit icon, created exit page

--- workoutapp/src/workoutapp/app.py
+++ workoutapp/src/workoutapp/app.py
@@ -16,36 +16,38 @@ def greeting(name):
 
 class WorkoutApp(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
 
         #Box Creation Section
         main_box = toga.Box(style=Pack(direction=COLUMN, flex=1, background_color="#15182e"))
         header_box = toga.Box(style=Pack(direction=COLUMN, background_color="#d6c724c1"))
 
-
         #Widget Creation Section
         header_label = toga.Label(f"Today - {datetime.now().strftime("%b %d %Y")}", style=Pack(font_size=24, font_weight="bold", padding=10, color="#182375"))
-        edit_button = toga.Button("Edit", on_press=self.say_hello, style=Pack(width=200, height=200, background_color="#182375", color="#d6c724c1"))
+        edit_button = toga.Button("Edit", on_press=self.show_edit_view, style=Pack(width=200, height=200, background_color="#182375", color="#d6c724c1"))
 
-
-        #Alt Box Build Section
-        header_box.add(header_label)
-
-        
-        #Main Box Build Section
+        #Box Build Section
         main_box.add(header_box)
         main_box.add(edit_button)
+        header_box.add(header_label)
+
+        #Window Build Section
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
-    async def say_hello(self, widget):
-        await self.main_window.dialog(toga.InfoDialog(greeting(self.name_input.value), "What it do man?",))
+    def show_edit_view(self, widget):
+        
+        #Box Creation Section
+        edit_box = toga.Box(style=Pack(direction=COLUMN, flex=1, background_color="#15182e"))
+
+        #Widget Creation Section
+        test_label = toga.Label("This is the edit view", style=Pack(font_size=24, font_weight="bold", padding=10, color="#182375"))
+
+        #Box Build Section
+        edit_box.add(test_label)
+
+        #Window Build Section
+        self.main_window.content = edit_box
 
 def main():
     return WorkoutApp()
